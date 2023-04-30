869b41b8ab3edd82d56bc249ad9b4f4b7499eb50
Clément Delafargue <clement@delafargue.name>
2023-05-01 00:43:43 +0200

    Store intermediate word counts

--- 750.sh
+++ 750.sh
@@ -2,17 +2,43 @@
 
 TODAY=$(date +'%Y-%m-%d')
 
+ensure_db() {
+  if [ ! -f "journal.db" ]
+  then
+    cat << EOF | sqlite3 journal.db
+create table counts (date text, timestamp datetime, count integer);
+EOF
+  fi
+}
+
 ensure_file() {
   if [ ! -f "${1}.md" ]
   then
     echo "# ${1}" > "${1}.md"
+    cat << EOF | sqlite3 journal.db
+insert into counts (date, timestamp, count) values ('${1}', '$(date +'%Y-%m-%d %H:%M:%S')', 0);
+EOF
   fi
 }
 
 count_words() {
   local word_count
+  local modified
+  local last_count
   word_count=$(tail -n+2 < "${1}.md"| wc -w)
+  modified=$(date -d "@$(stat -c '%Z' "${1}.md")" +'%Y-%m-%d %H:%M:%S')
+  last_count=$(cat << EOF | sqlite3 journal.db
+select count from counts where date = '${1}' order by timestamp desc limit 1
+EOF
+  ) 
   echo "${word_count} words"
+  if [ "$word_count" != "$last_count" ]
+  then
+    echo "Saving ${modified}"
+    cat << EOF | sqlite3 journal.db
+insert into counts (date, timestamp, count) values ('${1}', '${modified}', ${word_count})
+EOF
+  fi
 }
 
 
@@ -26,7 +52,14 @@ case "$1" in
   "count")
     count_words "${TODAY}"
     ;;
+  "ensure_file")
+    ensure_file "${TODAY}"
+    ;;
+  "initdb")
+    init_db
+    ;;
   *)
+    ensure_db
     ensure_file "${TODAY}"
     zellij --layout layout.kdl
     ;;
